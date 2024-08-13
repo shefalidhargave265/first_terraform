5c376803e70c431278efc5486bff98c27c6a79a1
Shefali Dhargave <129889473+shefalidhargave265@users.noreply.github.com>
2024-08-13 23:33:19 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-04a81a99f5ec58529"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform_Demo"
+  }
+}
+
+
+
+
+
+
+
